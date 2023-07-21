21fbd26468aea0ca1dd3c13af9bfc15c32a9ab7f
Oluwaseyifunmi Oyefeso <80963324+sheyzi@users.noreply.github.com>
2023-07-21 02:42:41 +0100

    Revert "Created Login and Signup UI"

--- src/routes/(unauthed)/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div class="h-full flex flex-col justify-center items-center">
-    <div>
-        <a class="btn variant-filled" href="/auth/login">Login</a>
-        <a class="btn variant-filled-primary" href="/auth/signup">Sign up</a>
-    </div>
-</div>
