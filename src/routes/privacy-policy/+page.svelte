155ce83583ff15b5150bd60980c466cd4787b3e7
Sheyzi <gistkiosk@gmail.com>
2023-07-25 10:33:50 +0100

    Added privacy policy

---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,123 @@
+<svelte:head>
+	<title>Privacy Policy | Aujre</title>
+</svelte:head>
+
+<!-- <div class="max-w-6xl mx-auto px-5 md:px-10 my-5"> -->
+<div class="container mx-auto px-4 py-8">
+	<h1 class="text-4xl font-bold mb-4">Privacy Policy</h1>
+
+	<p class="mb-4">
+		Welcome to Aujre! At Aujre, we are committed to protecting your privacy and safeguarding your
+		personal information. This Privacy Policy outlines how we collect, use, and protect the data you
+		provide to us.
+	</p>
+
+	<div class="space-y-5">
+		<div>
+			<h2 class="text-xl font-semibold mb-4">Information We Collect:</h2>
+			<ol class="list-decimal ml-6 mb-6">
+				<li class="mb-4">
+					<h3 class="text-base font-semibold">Personal Information:</h3>
+					<p>
+						When you sign up for Aujre, we may collect certain personal information, such as your
+						name, email address, and contact details. This information is necessary for creating and
+						managing your Aujre account and providing you with access to our services.
+					</p>
+				</li>
+				<li class="mb-4">
+					<h3 class="text-base font-semibold">Usage Data:</h3>
+					<p>
+						We may automatically collect information about your interactions with Aujre, including
+						your IP address, device information, browser type, and usage patterns. This data helps
+						us analyze user behavior, improve our services, and enhance your overall experience.
+					</p>
+				</li>
+			</ol>
+		</div>
+		<div>
+			<h2 class="text-xl font-semibold mb-4">How We Use Your Information:</h2>
+			<ol class="list-decimal ml-6 mb-6">
+				<li class="mb-4">
+					<h3 class="text-base font-semibold">Providing Services:</h3>
+					<p>
+						We use your personal information to deliver, maintain, and improve our services,
+						ensuring you have a smooth and efficient experience on Aujre.
+					</p>
+				</li>
+				<li class="mb-4">
+					<h3 class="text-base font-semibold">Communication:</h3>
+					<p>
+						We may use your contact information to send you important updates, service
+						announcements, and other relevant information about Aujre.
+					</p>
+				</li>
+				<li class="mb-4">
+					<h3 class="text-base font-semibold">Personalization:</h3>
+					<p>
+						With your consent, we may personalize your experience on Aujre, including recommending
+						relevant features, updates, and content tailored to your preferences.
+					</p>
+				</li>
+			</ol>
+		</div>
+
+		<div>
+			<h2 class="text-xl font-semibold mb-4">Data Sharing:</h2>
+			<ol class="list-decimal ml-6 mb-6">
+				<li class="mb-4">
+					<h3 class="text-base font-semibold">Third-Party Service Providers:</h3>
+					<p>
+						We may share your personal information with trusted third-party service providers who
+						assist us in delivering our services, such as payment processors and email marketing
+						platforms. These providers are bound by confidentiality agreements and will only use
+						your data as necessary to fulfill their respective roles.
+					</p>
+				</li>
+				<li class="mb-4">
+					<h3 class="text-base font-semibold">Legal Requirements:</h3>
+					<p>
+						We may disclose your information to comply with legal obligations, enforce our Terms of
+						Service, and protect our rights, property, or safety, as required by law or in response
+						to a valid legal request.
+					</p>
+				</li>
+			</ol>
+		</div>
+
+		<div>
+			<h2 class="text-xl font-semibold mb-4">Data Security:</h2>
+			<p>
+				We take data security seriously and employ industry-standard measures to protect your
+				personal information. However, no method of data transmission over the internet or
+				electronic storage is completely secure. While we strive to protect your data, we cannot
+				guarantee absolute security.
+			</p>
+		</div>
+
+		<div>
+			<h2 class="text-xl font-semibold mb-4">Your Choices:</h2>
+			<p>
+				You have the right to access, update, or delete your personal information stored on Aujre.
+				You can manage your account settings and communication preferences within your Aujre account
+				or by contacting us at <a href="mailto:connect@aujre.com" class="underline"
+					>connect@aujre.com</a
+				>.
+			</p>
+		</div>
+
+		<div>
+			<h2 class="text-xl font-semibold mb-4">Changes to this Privacy Policy:</h2>
+			<p>
+				We may update this Privacy Policy from time to time to reflect changes in our practices or
+				legal requirements. We encourage you to review this policy regularly for any updates. Your
+				continued use of Aujre after the modifications signifies your acceptance of the updated
+				Privacy Policy.
+			</p>
+		</div>
+
+		<p>
+			If you have any questions or concerns about our Privacy Policy or data practices, please
+			contact us at <a href="mailto:connect@aujre.com" class="underline">connect@aujre.com</a>.
+		</p>
+	</div>
+</div>
